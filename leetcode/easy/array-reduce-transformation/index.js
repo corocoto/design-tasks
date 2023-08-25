f8a144d9d336ed1b690a84ce3c4daa08ac9168da
Artem Gusev <corocoto.art@gmail.com>
2023-08-25 20:25:30 +0300

    Adds solution onto "Array Reduce Transformation" task

---
+++ leetcode/easy/array-reduce-transformation/index.js
@@ -0,0 +1,17 @@
+// Link on the task: https://leetcode.com/problems/array-reduce-transformation/description/
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let accumulator = init;
+
+    for (let i = 0; i < nums.length; i++) {
+        accumulator = fn(accumulator, nums[i]);
+    }
+
+    return accumulator;
+};
