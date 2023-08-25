37a8fba82c0e67e5e5588922b2ad519a0e5528e4
Artem Gusev <corocoto.art@gmail.com>
2023-08-25 20:30:17 +0300

    Adds solution onto "Function Composition" task

---
+++ leetcode/easy/function-composition/index.js
@@ -0,0 +1,22 @@
+// Link on the task: https://leetcode.com/problems/function-composition/description/
+
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+	return function(x) {
+        let result = x;
+        
+        for (let i = functions.length - 1; i >= 0; i--) {
+            result = functions[i](result);
+        }
+
+        return result;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
