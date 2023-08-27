078006b2455a264c8a7495312217c4d3e2f47982
Artem Gusev <corocoto.art@gmail.com>
2023-08-27 16:47:36 +0300

    Adds solution onto "Promise Time Limit" task

---
+++ leetcode/medium/promise-time-limit/index.js
@@ -0,0 +1,20 @@
+// Link on the task: https://leetcode.com/problems/promise-time-limit/description/
+
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+	return async function(...args) {
+        return Promise.race([
+            fn(...args),
+            new Promise((_, reject) => setTimeout(reject, t, "Time Limit Exceeded"))
+        ]);
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
