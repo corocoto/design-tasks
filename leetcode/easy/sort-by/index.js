844cf504baeb0645c990d67804381370d11e27bf
Artem Gusev <corocoto.art@gmail.com>
2023-08-29 22:51:18 +0300

    Adds solution onto "Sort By" task

---
+++ leetcode/easy/sort-by/index.js
@@ -0,0 +1,10 @@
+// Linkt on the task: https://leetcode.com/problems/sort-by/description/
+
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+    return arr.sort((a,b) => fn(a) - fn(b));
+};
